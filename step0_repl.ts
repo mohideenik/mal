6414bd2cc26ec157d0a2133aab3222e2167b11dd
Mohideen Imran Khan <mohideenimran@outlook.com>
2018-11-27 19:52:57 +0800

    Initial commit with step0

---
+++ step0_repl.ts
@@ -0,0 +1,29 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function read(str: String): String {
+  return str;
+}
+
+function evaluate(str: String): String {
+    return str;
+}
+
+function prnt(str: String): String {
+    return str;
+}
+
+function rep(str: String): String {
+    return str;
+}
+
+// Main loop
+process.stdout.write("user> ")
+rl.on('line', (input: String) => {
+    console.log(rep(input))
+    process.stdout.write("user> ")
+});
